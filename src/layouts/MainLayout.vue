3343be8425ebe1cb2ab9919bf5a4fb026a33fabe
Дмитрий Лакутин <lana.lakutina@mail.ru>
2024-04-11 16:58:51 +0300

    basic page layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,9 +1,123 @@
 <template>
-  <q-layout> </q-layout>
+  <q-layout view="lHr Lpr fff">
+    <q-header reveal elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+
+        <q-toolbar-title>
+          <q-btn
+            to="/"
+            text-color="accent"
+            label="InkFinder"
+            push
+            icon="brush"
+          />
+        </q-toolbar-title>
+
+        <q-btn
+          v-if="false"
+          dense
+          flat
+          round
+          icon="menu"
+          @click="toggleRightDrawer"
+        />
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
+      <q-scroll-area class="fit">
+        <q-list>
+          <template v-for="(menuItem, index) in menuList" :key="index">
+            <q-item clickable :active="menuItem.label === 'Outbox'" v-ripple>
+              <q-item-section avatar>
+                <q-icon :name="menuItem.icon" />
+              </q-item-section>
+              <q-item-section>
+                {{ menuItem.label }}
+              </q-item-section>
+            </q-item>
+            <q-separator :key="'sep' + index" v-if="menuItem.separator" />
+          </template>
+        </q-list>
+      </q-scroll-area>
+    </q-drawer>
+
+    <q-drawer
+      v-if="false"
+      show-if-above
+      v-model="rightDrawerOpen"
+      side="right"
+      bordered
+    >
+      <!-- drawer content -->
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer bordered class="bg-secondary text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
+          </q-avatar>
+          <div>Title</div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+  </q-layout>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
+
+const leftDrawerOpen = ref(false);
+const rightDrawerOpen = ref(false);
+
+const menuList = [
+  {
+    icon: 'account_circle',
+    label: 'Личный кабинет',
+    separator: false,
+  },
+  {
+    icon: 'inbox',
+    label: 'Анкеты',
+    separator: false,
+  },
+  {
+    icon: 'send',
+    label: 'Чаты',
+    separator: false,
+  },
+  {
+    icon: 'color_lens',
+    label: 'Татуировки',
+    separator: true,
+  },
+  {
+    icon: 'settings',
+    label: 'Настройки',
+    separator: false,
+  },
+  {
+    icon: 'feedback',
+    label: 'Отправить Feedback',
+    separator: false,
+  },
+  {
+    icon: 'help',
+    iconColor: 'primary',
+    label: 'Помощь',
+    separator: false,
+  },
+];
+
+const toggleLeftDrawer = () => (leftDrawerOpen.value = !leftDrawerOpen.value);
+const toggleRightDrawer = () =>
+  (rightDrawerOpen.value = !rightDrawerOpen.value);
 
 defineOptions({
   name: 'MainLayout',
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,11 +1,162 @@
 <template>
-  <q-page class="row items-center justify-evenly"> </q-page>
+  <q-page class="row justify-evenly">
+    <div class="row q-mt-xl">
+      <template v-for="definition in inkFinderDefinitions" :key="definition">
+        <q-card dark bordered class="col q-mx-sm bg-primary">
+          <q-img :src="`https://random.imagecdn.app/500/200`" />
+          <q-card-section>
+            <div class="text-h6">{{ definition.title }}</div>
+            <div class="text-subtitle2">{{ definition.subtitle }}</div>
+          </q-card-section>
+
+          <q-separator dark inset />
+
+          <q-card-section> {{ definition.text }} </q-card-section>
+        </q-card>
+      </template>
+    </div>
+
+    <q-timeline class="q-px-md" color="secondary">
+      <q-timeline-entry heading> Timeline heading </q-timeline-entry>
+
+      <q-timeline-entry title="Event Title" subtitle="February 22, 1986">
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+      </q-timeline-entry>
+
+      <q-timeline-entry
+        title="Event Title"
+        subtitle="February 21, 1986"
+        icon="delete"
+      >
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+      </q-timeline-entry>
+
+      <q-timeline-entry heading> November, 2017 </q-timeline-entry>
+
+      <q-timeline-entry
+        title="Event Title"
+        subtitle="February 22, 1986"
+        avatar="https://cdn.quasar.dev/img/avatar2.jpg"
+      >
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+      </q-timeline-entry>
+
+      <q-timeline-entry title="Event Title" subtitle="February 22, 1986">
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+      </q-timeline-entry>
+
+      <q-timeline-entry
+        title="Event Title"
+        subtitle="February 22, 1986"
+        color="orange"
+        icon="done_all"
+      >
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+      </q-timeline-entry>
+
+      <q-timeline-entry title="Event Title" subtitle="February 22, 1986">
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+      </q-timeline-entry>
+
+      <q-timeline-entry title="Event Title" subtitle="February 22, 1986">
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+      </q-timeline-entry>
+    </q-timeline>
+  </q-page>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-
+let inkFinderDefinitions = [
+  {
+    title: 'Our Changing Planet',
+    subtitle: 'by John Doe',
+    text: 'Чё-то надо написать',
+  },
+  {
+    title: 'Our Changing Planet',
+    subtitle: 'by John Doe',
+    text: 'Чё-то надо написать',
+  },
+  {
+    title: 'Our Changing Planet',
+    subtitle: 'by John Doe',
+    text: 'Чё-то надо написать',
+  },
+  {
+    title: 'Our Changing Planet',
+    subtitle: 'by John Doe',
+    text: 'Чё-то надо написать',
+  },
+  {
+    title: 'Our Changing Planet',
+    subtitle: 'by John Doe',
+    text: 'Чё-то надо написать',
+  },
+  {
+    title: 'Our Changing Planet',
+    subtitle: 'by John Doe',
+    text: 'Чё-то надо написать',
+  },
+];
 defineOptions({
   name: 'IndexPage',
 });
 </script>
+
+<style scoped lang="scss"></style>
